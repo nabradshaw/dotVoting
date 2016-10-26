eeed9e5674a631a4703cfe2de07404a22a032294
Nate Bradshaw <nbradshaw@pillartechnology.com>
2016-10-26 11:28:04 -0400

    base config on springboot and hello world

---
+++ src/main/java/com/pillartechnology/academy/voting/DotVoting.java
@@ -0,0 +1,13 @@
+package com.pillartechnology.academy.voting;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotVoting {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+
+}
