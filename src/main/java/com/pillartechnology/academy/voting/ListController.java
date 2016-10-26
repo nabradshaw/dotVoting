18510eb784e515a8e0e8048d7fc51fef1c678699
Cody Knapp <cknapp@pillartechnology.com>
2016-10-26 16:37:08 -0400

    Adding list endpoint to return list of items for voting

---
+++ src/main/java/com/pillartechnology/academy/voting/ListController.java
@@ -0,0 +1,24 @@
+package com.pillartechnology.academy.voting;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+@RequestMapping("/api/list")
+public class ListController {
+
+    @RequestMapping(method = GET)
+    public List<String> getVotingList() {
+        List<String> result = new ArrayList<String>();
+        result.add("A");
+        result.add("B");
+        result.add("C");
+        result.add("D");
+        return result;
+    }
+}
---
+++ src/test/java/com/pillartechnology/academy/voting/ListControllerTestIT.java
@@ -0,0 +1,37 @@
+package com.pillartechnology.academy.voting;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.junit.Assert.fail;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ListControllerTestIT {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testListControllerEndpointReturnsOkStatus() throws Exception {
+        mockMvc.perform(get("/api/list")).andExpect(status().isOk());
+    }
+
+    @Test
+    public void testListControllerEndpointReturnsListOfStrings() throws Exception {
+        MvcResult result = mockMvc.perform(get("/api/list")).andReturn();
+        String resultContent = result.getResponse().getContentAsString();
+
+        if (!(resultContent.startsWith("[\"") && resultContent.endsWith("\"]") || resultContent.equals("[]"))) {
+            fail("A list was not returned by the list endpoint");
+        }
+    }
+}
