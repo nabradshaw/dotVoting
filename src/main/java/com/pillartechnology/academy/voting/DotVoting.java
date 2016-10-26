6c47889244f4482cef8fe8426cd67e87e7605265
Cody Knapp <cknapp@pillartechnology.com>
2016-10-26 11:43:33 -0400

    Added REST endpoint for hello world

--- src/main/java/com/pillartechnology/academy/voting/DotVoting.java
+++ src/main/java/com/pillartechnology/academy/voting/DotVoting.java
@@ -1,13 +1,14 @@
 package com.pillartechnology.academy.voting;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DotVoting {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-
+        SpringApplication.run(DotVoting.class, args);
+        System.out.println("DotVoting has started");
     }
 
 }
