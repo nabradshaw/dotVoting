4ae06ce0c13ccf68d31136926956a48d72293bab
Cody Knapp <cknapp@pillartechnology.com>
2016-10-27 12:50:30 -0400

    Style changes and adding ID for poll items

--- src/main/java/com/pillartechnology/academy/voting/model/PollItemModel.java
+++ src/main/java/com/pillartechnology/academy/voting/model/PollItemModel.java
@@ -1,6 +1,8 @@
 package com.pillartechnology.academy.voting.model;
 
 public class PollItemModel {
+    private String id;
+    private int voteCount;
     private String description;
 
     public int getVoteCount() {
@@ -11,13 +13,19 @@ public class PollItemModel {
         this.voteCount = voteCount;
     }
 
-    private int voteCount;
-
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 }
