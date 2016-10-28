d76fdb953d86fa36e9d420b49cb76be98af3c1d1
Nicholas Beach <nbeach@pillartechnology.com>
2016-10-28 13:46:20 -0400

    Removed unnecessary tests, logic was moved into the service

--- src/main/java/com/pillartechnology/academy/voting/controller/ListController.java
+++ src/main/java/com/pillartechnology/academy/voting/controller/ListController.java
@@ -1,6 +1,5 @@
 package com.pillartechnology.academy.voting.controller;
 
-import com.pillartechnology.academy.voting.model.PollItemModel;
 import com.pillartechnology.academy.voting.model.PollModel;
 import com.pillartechnology.academy.voting.service.PollService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
@@ -32,14 +29,6 @@ public class ListController {
 
     @RequestMapping(method = POST, value = "/api/list")
     public void savePoll(@RequestBody PollModel poll) {
-        poll.setId("1");
-        List<PollItemModel> items = poll.getPollItems();
-        if(items != null) {
-            for(int i = 0; i < items.size(); i++) {
-                items.get(i).setId(String.valueOf(i));
-            }
-        }
-
         pollService.savePoll(poll);
     }
 }
--- src/test/java/com/pillartechnology/academy/voting/controller/ListControllerTest.java
+++ src/test/java/com/pillartechnology/academy/voting/controller/ListControllerTest.java
@@ -1,7 +1,6 @@
 package com.pillartechnology.academy.voting.controller;
 
 
-import com.pillartechnology.academy.voting.model.PollItemModel;
 import com.pillartechnology.academy.voting.model.PollModel;
 import com.pillartechnology.academy.voting.service.PollService;
 import org.junit.Before;
@@ -10,9 +9,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.verify;
@@ -49,33 +45,6 @@ public class ListControllerTest {
 
         verify(service).savePoll(pollCaptor.capture());
         assertThat(pollCaptor.getValue()).isSameAs(expected);
-    }
-
-    @Test
-    public void savePoll_SetsTheIdForThePoll() {
-        PollModel expected = new PollModel();
-        ArgumentCaptor<PollModel> pollCaptor = ArgumentCaptor.forClass(PollModel.class);
-
-        uut.savePoll(expected);
-
-        verify(service).savePoll(pollCaptor.capture());
-        assertThat(pollCaptor.getValue().getId()).isEqualTo("1");
-    }
-
-    @Test
-    public void savePoll_SetsTheIdsForThePollItems() {
-        PollModel expected = new PollModel();
-        expected.setPollItems(Arrays.asList(new PollItemModel(), new PollItemModel(), new PollItemModel()));
-        ArgumentCaptor<PollModel> pollCaptor = ArgumentCaptor.forClass(PollModel.class);
-
-        uut.savePoll(expected);
-
-        verify(service).savePoll(pollCaptor.capture());
-
-        List<PollItemModel> result = pollCaptor.getValue().getPollItems();
-        for(int i = 0; i < result.size(); i++) {
-            assertThat(result.get(i).getId()).isEqualTo(String.valueOf(i));
-        }
     }
 
     @Test
